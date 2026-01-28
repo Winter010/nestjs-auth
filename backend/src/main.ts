806863dcb24bfd175b883b2bfa93c3355e868db7
Winter010 <kia200646@gmail.com>
2026-01-28 22:38:11 +1000

    back: add CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,12 @@ import { AppModule } from './app.module'
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+
   app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(process.env.PORT ?? 3001)
